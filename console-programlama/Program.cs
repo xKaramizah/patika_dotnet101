d952ddb88115630228c0cf6840f64aacb6625efc
Emre CAN <i.e.can@yandex.com>
2024-01-23 01:11:01 +0300

    first commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Adınız: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hoşgeldiniz" + " " + name);
+
+        
+    }
+}
---
+++ variable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace variable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        DateTime dt = DateTime.Now;
+        //Console.WriteLine(dt);
+
+        string dt1 = DateTime.Now.ToString();
+        Console.WriteLine(dt1);
+        string dt2 = DateTime.Now.ToString("dd.MM.yyyy");
+        Console.WriteLine(dt2);
+        string dt3 = DateTime.Now.ToString("HH:mm");
+        Console.WriteLine(dt3);
+    }
+}
